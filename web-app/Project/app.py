eeee9695ae4d8d438c27307cae2cd22de4ce9728
Chris Buckner <christopher.d.buckner@gmail.com>
2020-02-25 23:00:43 -0600

    added ability to serve content correctly via nginx and web-app. ws-6

--- web-app/Project/app.py
+++ web-app/Project/app.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from starlette.templating import Jinja2Templates
+from starlette.requests import Request
 
 app = FastAPI()
 
-@app.get('/blog')
-async def blog():
-    return {'blog': 'web-app blog'}
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get('/web-server/blog')
+async def get_blog(request: Request):
+    return templates.TemplateResponse("blog.html", {'request' : request})
